213cf781384fefb52f2b3ff752e3df4b46114428
Gonzalo Baliarda <gbaliarda@itba.edu.ar>
2021-09-05 00:21:42 -0300

    Code modularization

--- src/master.c
+++ src/master.c
@@ -11,19 +11,37 @@
 #define MAX_INITIAL_FILES 1
 #define MAX_OUT_LEN 512
 
+int initializeSlaves(int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2], int fileCount);
+int setInitialFiles(int slaveCount, int filePipe[MAX_SLAVE_QTY][2], char const* files[], int fileCount);
+void monitorSlaves(char const* files[], int fileCount, int slaveCount, int taskNum, int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2]);
+void readResultPipe(int resultPipeEnd);
+void giveAnotherTask(int filePipeEnd, const char *file);
+void closePipes(int slaveCount, int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2]);
+void manageNewResults(int *completedTasks, int slaveCount, int *taskNum, const char *files[], int fileCount, int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2], fd_set fdSlaves);
+
 int main (int argc, char const *argv[]) {
+
   if (argc < 2) {
     printf("Please provide the files to process.\n");
     exit(-1);
   }
 
-  pid_t pid;
   int filePipe[MAX_SLAVE_QTY][2];
   int resultPipe[MAX_SLAVE_QTY][2];
 
-  //Crear esclavos
+  int slaveCount = initializeSlaves(filePipe, resultPipe, argc-1);
+  int taskNum = setInitialFiles(slaveCount, filePipe, argv, argc);
+
+  monitorSlaves(argv, argc, slaveCount, taskNum, filePipe, resultPipe);
+  closePipes(slaveCount, filePipe, resultPipe);
+
+}
+
+int initializeSlaves(int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2], int fileCount) {
+  pid_t pid;
+
   int slaveCount = 0;
-  while (slaveCount < MAX_SLAVE_QTY && slaveCount < argc/MAX_INITIAL_FILES) {
+  while (slaveCount < MAX_SLAVE_QTY && slaveCount < (fileCount+1)/MAX_INITIAL_FILES) {
     pipe(filePipe[slaveCount]);
     pipe(resultPipe[slaveCount]);
 
@@ -32,14 +50,14 @@ int main (int argc, char const *argv[]) {
     if (pid == -1) {
       perror("fork");
       exit(-1);
-    } 
+    }
     else if (pid == 0) {
       close(filePipe[slaveCount][1]);
       close(resultPipe[slaveCount][0]);
       dup2(filePipe[slaveCount][0], STDIN);
       dup2(resultPipe[slaveCount][1], STDOUT);
-      char *argumentos[2] = {"/tmp", NULL};
-      execvp("./slave.out", argumentos);
+      char *args[2] = {"/tmp", NULL};
+      execvp("./slave.out", args);
       perror("execvp");
       exit(-1);
     }
@@ -50,57 +68,64 @@ int main (int argc, char const *argv[]) {
     }
   }
   
-  fd_set fdSlaves;
-  for(int nSlave = 0; nSlave < slaveCount; nSlave++)
-    FD_SET(resultPipe[nSlave][0], &fdSlaves);
+  return slaveCount;
+}
 
+int setInitialFiles(int slaveCount, int filePipe[MAX_SLAVE_QTY][2], char const* files[], int fileCount){
   int taskNum = 1;
-  // Reparto inicial de tareas
-  for (int i = 0; i < slaveCount; i++) {
-    for (int j = 0; j < MAX_INITIAL_FILES && taskNum < argc; j++) {
-      write(filePipe[i][1], argv[taskNum], strlen(argv[taskNum]));
+
+  for (int i = 0; i < slaveCount; i++)
+    for (int j = 0; j < MAX_INITIAL_FILES && taskNum < fileCount; j++) {
+      write(filePipe[i][1], files[taskNum], strlen(files[taskNum]));
       write(filePipe[i][1], "\n", 1);  
       taskNum++;
     }
-  }
 
+  return taskNum;
+}
+
+void monitorSlaves(char const* files[], int fileCount, int slaveCount, int taskNum, int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2]) {
+  fd_set fdSlaves;
   int completedTasks = 0;
 
-  while (completedTasks < argc-1) {
-    
-    // lees cuando alguno termina
-    int slavesReady = select(resultPipe[slaveCount-1][1], &fdSlaves, NULL, NULL, NULL);
-    if (slavesReady == -1)
-      perror("select");
-    else {
-
-      for(int nSlave = 0; nSlave < slaveCount; nSlave++) {
-        if(FD_ISSET(resultPipe[nSlave][0], &fdSlaves)) {
-          completedTasks++;
-          char buff[MAX_OUT_LEN];
-          int readCount = read(resultPipe[nSlave][0], buff, MAX_OUT_LEN);
-          buff[readCount] = 0;
-          printf("%s\n", buff);
-
-          if (taskNum < argc) {
-            write(filePipe[nSlave][1], argv[taskNum], strlen(argv[taskNum]));
-            write(filePipe[nSlave][1], "\n", 1);  
-            taskNum++;
-          }
-        }
-      }
-    }
-      
+  while (completedTasks < fileCount-1) {
     FD_ZERO(&fdSlaves);
     for(int nSlave = 0; nSlave < slaveCount; nSlave++)
       FD_SET(resultPipe[nSlave][0], &fdSlaves);
+    
+    if (select(resultPipe[slaveCount-1][1], &fdSlaves, NULL, NULL, NULL) == -1)
+      perror("select");
+    else
+      manageNewResults(&completedTasks, slaveCount, &taskNum, files, fileCount, filePipe, resultPipe, fdSlaves);
   }
+}
 
+void manageNewResults(int *completedTasks, int slaveCount, int *taskNum, const char *files[], int fileCount, int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2], fd_set fdSlaves) {
+  for(int nSlave = 0; nSlave < slaveCount; nSlave++)
+    if (FD_ISSET(resultPipe[nSlave][0], &fdSlaves)) {
+      (*completedTasks)++;
+      readResultPipe(resultPipe[nSlave][0]);
+
+      if (*taskNum < fileCount)
+        giveAnotherTask(filePipe[nSlave][1], files[(*taskNum)++]);
+    }
+}
+
+void readResultPipe(int resultPipeEnd) {
+  char buff[MAX_OUT_LEN];
+  int readCount = read(resultPipeEnd, buff, MAX_OUT_LEN);
+  buff[readCount] = 0;
+  printf("%s\n", buff);
+}
+
+void giveAnotherTask(int filePipeEnd, const char *file) {
+  write(filePipeEnd, file, strlen(file));
+  write(filePipeEnd, "\n", 1);
+}
+
+void closePipes(int slaveCount, int filePipe[MAX_SLAVE_QTY][2], int resultPipe[MAX_SLAVE_QTY][2]){
   for (int i = 0; i < slaveCount; i++) {
     close(resultPipe[i][0]);
     close(filePipe[i][1]);
   }
-  
-  waitpid(pid, NULL, 0);
-
 }
--- src/slave.c
+++ src/slave.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define STDIN 0
-#define STDOUT 1
-#define MAX_LEN 8192
-#define SAT_SOLVER "minisat"
-
 #include <unistd.h>
 
-// Recibe los paths de el/los archivos a procesar. Tiene que recibir los paths por stdin e imprimir por stdout, despues se cambian los fd a los pipes correspondientes en el master.
+#define STDIN 0
+#define STDOUT 1
+#define MAX_LEN 1024
+#define SAT_SOLVER "minisat"
+
+FILE *executeSolver(char *fileName);
+void printResult(FILE *outputStream);
 
 int main(int argc, char const *argv[]) {
-  /*
-  Para pasar muchos paths de una podemos hacer "path1\npath2\n\path3" y luego ir recopilando los distintos paths con getline(), checkeando errno para ver si llegamos al final del archivo (EOF).
-  */
 
   char *line = NULL;
   size_t len = 0;
@@ -21,22 +19,33 @@ int main(int argc, char const *argv[]) {
   while ((nread = getline(&line, &len, stdin)) > 0) {
     line[nread-1] = 0;
     
-    char command[MAX_LEN + 1];
-    char output[MAX_LEN + 1];
-    sprintf(command, "%s %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", SAT_SOLVER, line);
-    
-    FILE *outputStream;
-    if ((outputStream = popen(command, "r")) == NULL)
-      perror("popen");
-
-    int count = fread(output, sizeof(char), MAX_LEN, outputStream);
-    output[count] = 0;
-
-    write(STDOUT, output, count);
+    FILE *outputStream = executeSolver(line);
+    printResult(outputStream);
 
     if (pclose(outputStream) == -1)
       perror("pclose");
   }
 
   free(line);
+  return 1;
+}
+
+FILE *executeSolver(char *fileName) {
+  char command[MAX_LEN + 1];
+  sprintf(command, "%s %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", SAT_SOLVER, fileName);
+  
+  FILE *outputStream;
+  if ((outputStream = popen(command, "r")) == NULL)
+    perror("popen");
+
+  return outputStream;
+}
+
+void printResult(FILE *outputStream) {
+  char output[MAX_LEN + 1];
+
+  int count = fread(output, sizeof(char), MAX_LEN, outputStream);
+  output[count] = 0;
+
+  write(STDOUT, output, count);
 }
